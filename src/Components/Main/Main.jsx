1830f6fd6a28b45ffa0941137aa9bb90b5b462d4
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-05 11:57:45 -0500

    adding loop to apply frame refresh

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import style from "./Main.module.css";
+import { useState } from "react";
+import { controller } from "./controllers";
+import Bird from "./Bird";
+import { useEffect } from "react";
+
+const bird = new Bird();
 
 export default function Main() {
+  const [birdPosition, setBirdPosition] = useState(0);
+
+  useEffect(() => {
+    let timeID;
+
+    if (!(birdPosition > 550)) {
+      timeID = setInterval(() => {
+        if (bird.lift === 0) {
+          bird.y += bird.gravity * bird.dificulty * 2;
+        } else {
+          bird.y -= bird.lift;
+          bird.lift--;
+        }
+
+        setBirdPosition(bird.y);
+      }, 24);
+    }
+
+    return () => {
+      clearInterval(timeID);
+    };
+  });
+
   return (
-    <div className={style.container}>
-      <div className={style.bird}></div>
+    <div
+      tabIndex={0}
+      onKeyDown={(e) => controller(e, bird)}
+      className={style.container}
+    >
+      <div className={style.fpsCount}>FPS: {60}</div>
+      <div className={style.bird} style={{ top: birdPosition }}></div>
       <div className={style.structure}>d</div>
     </div>
   );
