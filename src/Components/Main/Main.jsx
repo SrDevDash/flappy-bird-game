a58caa9236e335b45327e07a8c9ef432ebf9a85b
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-05 10:20:45 -0500

    adding basic component

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import style from "./Main.module.css";
+
+export default function Main() {
+  return (
+    <div className={style.container}>
+      <div className={style.bird}></div>
+      <div className={style.structure}>d</div>
+    </div>
+  );
+}
