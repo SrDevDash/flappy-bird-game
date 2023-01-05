d2b3d96195429ee2ac0ed1eb66cac33f6778f505
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-05 11:57:24 -0500

    Bird Controller

---
+++ src/Components/Main/controllers.js
@@ -0,0 +1,22 @@
+
+
+const controller = (e, bird) => {
+    switch (e.key) {
+        case 's':
+            jump(bird);
+            break
+
+        default:
+    }
+}
+
+const jump = (bird) => {
+    if (!(bird.y < 50)) {
+        bird.lift = 11;
+    }
+}
+
+
+module.exports = {
+    controller
+}
