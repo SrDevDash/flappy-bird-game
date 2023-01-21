7a2eb1bef91b4902d944d92c55ff42f0d5745259
Pedro Munoz <Pedrojose0272@gmail.com>
2023-01-21 18:18:35 -0500

    controler data to learn

--- src/Components/Main/controllers.js
+++ src/Components/Main/controllers.js
@@ -1,11 +1,13 @@
 
 
-const controller = (e, bird) => {
+const controller = (e, bird, dataToLearn, input) => {
     switch (e.key) {
         case 'w' || 'enter':
+            dataToLearn.push({ input, output: "Jump" });
             jump(bird);
             break
         case 's':
+            dataToLearn.push({ input, output: "Down" });
             downFast(bird);
             break
 
@@ -14,12 +16,15 @@ const controller = (e, bird) => {
 }
 
 const jump = (bird) => {
+    console.log('jump');
+
     if (!(bird.y < 50)) {
         bird.lift = 11;
     }
 }
 
 const downFast = (bird) => {
+    console.log('down')
     bird.velocity += 1;
 }
 
